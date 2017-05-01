f2c19310576231803dabc5c8256296a0d378afd8
Catatonica <izenka666@gmail.com>
2017-05-01 20:30:48 +0300

    first commit

---
+++ app/src/main/java/com/izyasosha/amoeba/AmoebaActivity.java
@@ -0,0 +1,13 @@
+package com.izyasosha.amoeba;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AmoebaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amoeba);
+    }
+}
